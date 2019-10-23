f8ccebb917c31dc06420f493ca59115052070762
BuiTuanVu <vubui2010@gmail.com>
2019-10-23 21:34:30 +0700

    Updates

---
+++ precache-manifest.15973ba68c3594cd9ebdb7501cf0c129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3324ddfec7a0ede6ce6d0cb1e7a7b95",
+    "url": "/xo-project/index.html"
+  },
+  {
+    "revision": "736388322f80483637f7",
+    "url": "/xo-project/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b8c2915010d5d3989b17",
+    "url": "/xo-project/static/css/main.b9f957c9.chunk.css"
+  },
+  {
+    "revision": "736388322f80483637f7",
+    "url": "/xo-project/static/js/2.0e720ae3.chunk.js"
+  },
+  {
+    "revision": "b8c2915010d5d3989b17",
+    "url": "/xo-project/static/js/main.ff1da581.chunk.js"
+  },
+  {
+    "revision": "75d97b1670d9db9c54bf",
+    "url": "/xo-project/static/js/runtime-main.8555ebf3.js"
+  }
+]);
